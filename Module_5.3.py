0e69aa223ad4bca5236aa61ae616864f41db8d2b
pelmen32110 <44937124+pelmen32110@users.noreply.github.com>
2024-11-03 16:36:40 +0300

    Add files via upload

---
+++ Module_5.3.py
@@ -0,0 +1,95 @@
+'''
+Необходимо дополнить класс House следующими специальными методами:
+1. __eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
+2. Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать результаты сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
+3. __add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+4. __radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+Остальные методы арифметических операторов, где self - x, other - y:
+
+Следует заметить, что other может быть не только числом, но и вообще любым объектом другого класса.
+Для более точной логики работы методов __eq__, __add__  и других методов сравнения и арифметики перед выполняемыми действиями лучше убедиться в принадлежности к типу при помощи функции isinstance:
+isinstance(other, int) - other указывает на объект типа int.
+isinstance(other, House) - other указывает на объект типа House.
+'''
+class House:
+    def __init__(self, name,numbers_of_floors): #количество этажей в доме
+        self.name = name
+        self.numbers_of_floors = numbers_of_floors
+
+    def go_to(self,new_floor): #переход на этаж
+        if self.numbers_of_floors < new_floor or new_floor < 1:
+            print("Такого этажа не существует")
+            return None
+        for i in range(new_floor+1):
+            print(i)
+
+    def __len__(self): #проверка высоты дома
+        return self.numbers_of_floors
+
+    def __str__(self): #вывод информации о доме
+        return f'Название: {self.name}, количество этажей {self.numbers_of_floors}'
+
+    def __eq__(self, other): # сравнение ==
+        if isinstance(other, int):
+            return House(self.numbers_of_floors == other.numbers_of_floors)
+
+    def __lt__(self, other): # сравнение <
+        if isinstance(other, int):
+            return House(self.numbers_of_floors < other.numbers_of_floors)
+
+    def __le__(self, other): # сравнение <=
+        if isinstance(other, int):
+            return House(self.numbers_of_floors <= other.numbers_of_floors)
+
+    def __gt__(self, other): # сравнение >
+        if isinstance(other, int):
+            return House(self.numbers_of_floors > other.numbers_of_floors)
+
+    def __ge__(self, other): # сравнение =>
+        if isinstance(other, int):
+            return House(self.numbers_of_floors >= other.numbers_of_floors)
+
+    def __ne__(self, other): # сравнение !=
+        if isinstance(other, int):
+            return House(self.numbers_of_floors != other.numbers_of_floors)
+
+    def __add__(self, value): # добавление новых этажей
+        if isinstance(value, House):
+            self.numbers_of_floors = self.numbers_of_floors + value
+        else:
+            print("Правый операнд должен быть типом int")
+        return self
+
+    def __radd__(self, value): # так же, как __add__()
+        return self.__add__(value)
+
+    def __iadd__(self, value): # так же, как __add__()
+        if isinstance(value, House):
+            self.numbers_of_floors += value
+        else:
+            print("Правый операнд должен быть типом int")
+        return self
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
